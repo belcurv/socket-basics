60d81ede1e91444b87fa6ba5010429c3d0ce2e21
JayS <jrschwane@uwalumni.com>
2016-02-27 16:08:27 -0600

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,10 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to Socket.io server!');
 });
+
+// frontend listens for an event called message, and when it gets that event
+// it prints it to the screen
+socket.on('message', function (message) {
+    console.log('New message: ');
+    console.log(message.text);
+})
--- server.js
+++ server.js
@@ -18,10 +18,28 @@ app.use(express.static(__dirname + '/public'));
 
 // on method lets us listen for events.  Takes 2 arguments, the event we're
 // listening for and a callback when it happens.
-io.on('connection', function () {
+// We get access to individual connected sockets.
+io.on('connection', function (socket) {
     console.log('User connected via socket.io');
-})
-
+    
+    // allow 2 browsers to talk to each ohter
+    // emit every message that comes in
+    socket.on('message', function (message) {
+        console.log('Message received: ' + message.text);
+        
+        // to broadcast options:
+        //   io.emit = sends message to everyone including the sender
+        //   broadcast.emit = sends it to everone EXCEPT the sender
+        socket.broadcast.emit('message', message);
+        
+    });
+    
+    
+    // emit method takes 2 args: even name and data to send
+    socket.emit('message', {
+        text: 'Welcome to the chat application'
+    });
+});
 
 
 http.listen(port, function () {
